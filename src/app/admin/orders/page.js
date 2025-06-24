c421e6797c8ef13a552e782a06e7914de6b504bd
AProd18 <aleksandar_prod18@hotmail.com>
2025-06-24 19:27:19 +0200

    feat: update admin orders page to show new address fields

--- src/app/admin/orders/page.js
+++ src/app/admin/orders/page.js
@@ -36,8 +36,12 @@ export default async function AdminOrdersPage() {
               <th className="p-2 border">Korisnik</th>
               <th className="p-2 border">Email</th>
               <th className="p-2 border">Broj tabli</th>
-              <th className="p-2 border">Telefon</th>
+              <th className="p-2 border">Drzava</th>
+              <th className="p-2 border">Postanski broj</th>
+              <th className="p-2 border">Grad</th>
               <th className="p-2 border">Adresa</th>
+              <th className="p-2 border">Ime i prezime</th>
+              <th className="p-2 border">Kontakt telefon</th>
               <th className="p-2 border">Napomena</th>
               <th className="p-2 border">Datum</th>
             </tr>
@@ -48,8 +52,12 @@ export default async function AdminOrdersPage() {
                 <td className="p-2 border">{order.user?.name || "N/A"}</td>
                 <td className="p-2 border">{order.user?.email || "N/A"}</td>
                 <td className="p-2 border">{order.quantity}</td>
-                <td className="p-2 border">{order.phone}</td>
+                <td className="p-2 border">{order.country}</td>
+                <td className="p-2 border">{order.postalCode}</td>
+                <td className="p-2 border">{order.city}</td>
                 <td className="p-2 border">{order.address}</td>
+                <td className="p-2 border">{order.fullName}</td>
+                <td className="p-2 border">{order.phone}</td>
                 <td className="p-2 border">
                   {order.note ? order.note : <em>-</em>}
                 </td>
