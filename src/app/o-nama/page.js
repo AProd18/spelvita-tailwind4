8a6dfe4f385ca275ba4fab4f18c91de78f2866ad
AProd18 <aleksandar_prod18@hotmail.com>
2025-07-05 23:41:05 +0200

    fix(style): improve font size for about me page

--- src/app/o-nama/page.js
+++ src/app/o-nama/page.js
@@ -2,7 +2,7 @@ export default function AboutPage() {
   return (
     <section className="max-w-5xl mx-auto px-2 md:px-6  md:py-20 space-y-8">
       <div className="bg-gradient-to-b from-[color:var(--color-dark-olive)] to-[color:var(--color-light-olive)] rounded-lg shadow-lg p-5 md:p-10 text-[color:var(--color-cornsilk)]">
-        <p className="text-lg mb-6 leading-relaxed">
+        <p className="text-md mb-6 leading-relaxed">
           Zdravo svima, zovem se <strong>Sladjana Brankov</strong> i dolazim iz
           prelepe Vojvodine — Banata, iz mesta Vršac. Pre oko deset godina, moja
           ljubav prema prirodi i zdravoj ishrani navela me je da započnem
@@ -10,34 +10,34 @@ export default function AboutPage() {
           prirodno na naše trpeze.
         </p>
 
-        <p className="text-lg mb-6 leading-relaxed">
+        <p className="text-md mb-6 leading-relaxed">
           Koristim isključivo domaću speltu koja se uzgaja na vojvođanskim
           njivama, u organskoj proizvodnji, bez upotrebe pesticida i veštačkih
           đubriva. Ova pažljivo odabrana sirovina garantuje kvalitet i bogatstvo
           hranljivih sastojaka u svakom gutljaju.
         </p>
 
-        <p className="text-lg mb-6 leading-relaxed">
+        <p className="text-md mb-6 leading-relaxed">
           Speltu meljem ručno, na specijalizovanoj mašini, u malim serijama,
           kako bih sačuvao sve njene prirodne vrednosti i osigurao da proizvod
           bude svež i autentičan. Na taj način mogu da zadovoljim potrebe svoje
           porodice i svih koji cene kvalitet i prirodnost.
         </p>
 
-        <p className="text-lg mb-6 leading-relaxed">
+        <p className="text-md mb-6 leading-relaxed">
           Sok od spelte pravim od svežih listova biljke, ceđenjem u ranoj fazi
           rasta, kada je koncentracija hranljivih materija najviša. Vodim se
           filozofijom da priroda sama zna šta je najbolje, pa ne koristim
           nikakve dodatke ili konzervanse.
         </p>
 
-        <p className="text-lg mb-6 leading-relaxed">
+        <p className="text-md mb-6 leading-relaxed">
           Dobijeni sok sipam u kalupe za led, svaki kalup sadrži 28 pažljivo
           zamrznutih kockica koje čuvaju sve blagodeti prirode. Na ovaj način,
           možete ga koristiti u bilo koje doba godine, bez gubitka kvaliteta.
         </p>
 
-        <p className="text-lg leading-relaxed">
+        <p className="text-md leading-relaxed">
           Verujem da je zdravlje najveće bogatstvo, a moj cilj je da vam pružim
           proizvod koji će vam pomoći da negujete svoje telo i duh na potpuno
           prirodan i ukusan način. Hvala što ste deo ove priče!
