598b484cd81c3e469648c63ed334d2ff8f76eb5a
AProd18 <aleksandar_prod18@hotmail.com>
2025-07-10 23:39:20 +0200

    chore: trigger redeploy on Vercel with minor change

--- src/app/verify-email/page.js
+++ src/app/verify-email/page.js
@@ -3,6 +3,8 @@ import VerifyEmailClient from "./VerifyEmailClient";
 
 export default function VerifyEmailPage() {
   return (
+    // Minor comment for triggering deploy
+
     <Suspense fallback={<p>Učitavanje verifikacije...</p>}>
       <VerifyEmailClient />
     </Suspense>
